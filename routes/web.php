c6f60e94dda3fb3347d60c5624d25436ece5dbf8
anharsaja <anharmuk12@gmail.com>
2023-06-29 02:16:21 +0700

    e-commerce app first built

---
+++ routes/web.php
@@ -0,0 +1,42 @@
+<?php
+
+use App\Http\Livewire\Admin\AdminDashboardComponent;
+use App\Http\Livewire\CartComponent;
+use App\Http\Livewire\HomeComponent;
+use App\Http\Livewire\ShopComponent;
+use Illuminate\Support\Facades\Route;
+use App\Http\Livewire\CheckoutComponent;
+use App\Http\Livewire\User\UserDashboardComponent;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', HomeComponent::class)->name('home.index');
+
+Route::get('/shop', ShopComponent::class)->name('shop');
+
+Route::get('/cart', CartComponent::class)->name('shop.cart');
+
+Route::get('/checkout', CheckoutComponent::class)->name('shop.checkout');
+
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth'])->name('dashboard');
+
+Route::middleware(['auth'])->group(function(){
+    Route::get('/user/dashboard', UserDashboardComponent::class)->name('user.dashboard');
+});
+Route::middleware(['auth', 'authadmin'])->group(function(){
+    Route::get('/admin/dashboard', AdminDashboardComponent::class)->name('admin.dashboard');
+});
+
+
+require __DIR__.'/auth.php';

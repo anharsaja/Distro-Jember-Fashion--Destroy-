3c90db923ed7f2e2816ef1d60175c8084ab1203c
anharsaja <anharmuk12@gmail.com>
2023-06-29 22:18:28 +0700

    add factory to seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\Category::factory(6)->create();
+        \App\Models\Product::factory(16)->create();
     }
 }

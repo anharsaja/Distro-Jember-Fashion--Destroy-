7f0f298d86f20ad194e4587ea5a3e2a7ec62b9f2
anharsaja <anharmuk12@gmail.com>
2023-06-29 22:16:38 +0700

    add faker to product factory

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class ProductFactory extends Factory
@@ -13,8 +14,19 @@ class ProductFactory extends Factory
      */
     public function definition()
     {
+        $product_name = $this->faker->unique()->words($nb = 6, $asText = true);
+        $slug = Str::slug($product_name, '-');
         return [
-            //
+            'name' => $product_name,
+            'slug' => $slug,
+            'short_description' => $this->faker->text(200),
+            'description' => $this->faker->text(500),
+            'regular_price' => $this->faker->numberBetween(10, 500),
+            'SKU' => 'PRD' . $this->faker->unique()->numberBetween(100, 500),
+            'stock_status' => 'instock',
+            'quantity' => $this->faker->numberBetween(10, 50),
+            'image' => 'product-' . $this->faker->numberBetween(1, 16),
+            'category_id' => $this->faker->numberBetween(1, 5)
         ];
     }
 }

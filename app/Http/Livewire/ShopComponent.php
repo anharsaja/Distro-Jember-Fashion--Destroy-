1aac6a19f2424e5240eb94801fb4b7b6156a504f
anharsaja <anharmuk12@gmail.com>
2023-06-30 14:08:25 +0700

    shop feature and models img - imghover

--- app/Http/Livewire/ShopComponent.php
+++ app/Http/Livewire/ShopComponent.php
@@ -2,12 +2,16 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Product;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class ShopComponent extends Component
 {
+    use WithPagination;
     public function render()
     {
-        return view('livewire.shop-component');
+        $products = Product::paginate(12);
+        return view('livewire.shop-component', ['products' => $products]);
     }
 }

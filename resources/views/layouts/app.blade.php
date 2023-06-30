f95444ab99fe0bce30d8cf15a8fc9b2910926c8b
anharsaja <anharmuk12@gmail.com>
2023-07-01 02:12:28 +0700

    add turbolinks js

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,6 +14,7 @@
     <link rel="shortcut icon" type="image/x-icon" href="{{ asset('assets/imgs/theme/favicon.ico') }}">
     <link rel="stylesheet" href="{{ asset('assets/css/main.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/css/custom.css') }}">
+    <script src="{{ mix('js/app.js') }}"></script>
     @livewireStyles
 </head>
 

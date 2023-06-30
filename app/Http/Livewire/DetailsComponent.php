c178b561f70d16538d2feb301199ae74b03db867
anharsaja <anharmuk12@gmail.com>
2023-06-30 14:34:37 +0700

    details product feature

---
+++ app/Http/Livewire/DetailsComponent.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use App\Models\Product;
+use Livewire\Component;
+
+class DetailsComponent extends Component
+{
+    public $slug;
+    
+    public function mount($slug)
+    {
+        $this -> slug = $slug;
+    }
+    
+    public function render()
+    {
+        $product = Product::where('slug', $this->slug)->first();
+        return view('livewire.details-component', ['product' => $product]);
+    }
+}

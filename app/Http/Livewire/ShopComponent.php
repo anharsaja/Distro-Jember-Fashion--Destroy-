158a2fe72d3b5101592dd6059d09eda961c4b00b
anharsaja <anharmuk12@gmail.com>
2023-07-01 13:10:40 +0700

    add to cart feature

--- app/Http/Livewire/ShopComponent.php
+++ app/Http/Livewire/ShopComponent.php
@@ -5,10 +5,19 @@ namespace App\Http\Livewire;
 use App\Models\Product;
 use Livewire\Component;
 use Livewire\WithPagination;
+use Cart;
 
 class ShopComponent extends Component
 {
     use WithPagination;
+
+    public function store($product_id, $product_name, $product_price)
+    {
+        Cart::add($product_id, $product_name, 1, $product_price)->associate('App\Models\Product');
+        session()->flash('success_message', 'Item add in cart');
+        return redirect()->route('shop.cart');
+    }
+    
     public function render()
     {
         $products = Product::paginate(12);

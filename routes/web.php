97447cb2594c699b014982f05c3d845363c82091
anharsaja <anharmuk12@gmail.com>
2023-07-02 04:04:20 +0700

    searching logics

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,7 @@ use App\Http\Livewire\ShopComponent;
 use Illuminate\Support\Facades\Route;
 use App\Http\Livewire\CheckoutComponent;
 use App\Http\Livewire\DetailsComponent;
+use App\Http\Livewire\SearchComponent;
 use App\Http\Livewire\User\UserDashboardComponent;
 
 /*
@@ -32,6 +33,8 @@ Route::get('/cart', CartComponent::class)->name('shop.cart');
 Route::get('/checkout', CheckoutComponent::class)->name('shop.checkout');
 
 Route::get('/product-category/{slug}', CategoryComponent::class)->name('product.category');
+
+Route::get('/search', SearchComponent::class)->name('product.search');
 
 // Route::get('/dashboard', function () {
 //     return view('dashboard');

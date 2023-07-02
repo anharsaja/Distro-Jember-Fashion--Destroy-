0fdccaf6ec78d2c9b0a148bb9af6f6645cfafb63
anharsaja <anharmuk12@gmail.com>
2023-07-02 20:45:59 +0700

    filter by model (scripts with ajax)

--- resources/views/livewire/shop-component.blade.php
+++ resources/views/livewire/shop-component.blade.php
@@ -146,15 +146,15 @@
                         <!-- Fillter By Price -->
                         <div class="sidebar-widget price_range range mb-30">
                             <div class="widget-header position-relative mb-20 pb-10">
-                                <h5 class="widget-title mb-10">Fill by price</h5>
+                                <h5 class="widget-title mb-10">Fillter by price</h5>
                                 <div class="bt-1 border-color-1"></div>
                             </div>
                             <div class="price-filter">
                                 <div class="price-filter-inner">
-                                    <div id="slider-range"></div>
+                                    <div id="slider-range" wire:ignore></div>
                                     <div class="price_slider_amount">
                                         <div class="label-input">
-                                            <span>Range:</span><input type="text" id="amount" name="price" placeholder="Add Your Price">
+                                            <span>Range:</span><strong><span class="text-info">${{ $min_value }}</span> - <span class="text-info">${{ $max_value }}</span></strong>
                                         </div>
                                     </div>
                                 </div>
@@ -251,3 +251,23 @@
         </section>
     </main>
 </div>
+
+@push('scripts')
+<script>
+    var sliderrange = $('#slider-range');
+    var amountprice = $('#amount');
+    $(function() {
+        sliderrange.slider({
+            range: true,
+            min: 0,
+            max: 1000,
+            values: [0, 1000],
+            slide: function(event, ui) {
+                // amountprice.val("$" + ui.values[0] + " - $" + ui.values[1]);
+                @this.set('min_value',ui.values[0]);
+                @this.set('max_value',ui.values[1]);
+            }
+        });
+    });
+</script>
+@endpush

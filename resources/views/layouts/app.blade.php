68b3678716d198209ee593f5cca2563d9db00b81
anharsaja <anharmuk12@gmail.com>
2023-07-02 20:45:20 +0700

    add scripts

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -708,6 +708,7 @@
     <script src="{{ asset('assets/js/main.js?v=3.3') }}"></script>
     <script src="{{ asset('assets/js/shop.js?v=3.3') }}"></script>
     @livewireScripts
+    @stack('scripts')
 </body>
 
 </html>

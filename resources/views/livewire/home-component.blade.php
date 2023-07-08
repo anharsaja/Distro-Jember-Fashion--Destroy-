14ca63e1e046306484001b485644806ac61be757
anharsaja <anharmuk12@gmail.com>
2023-07-08 13:40:37 +0700

    change banner

--- resources/views/livewire/home-component.blade.php
+++ resources/views/livewire/home-component.blade.php
@@ -1034,7 +1034,7 @@
                     <img src="assets/imgs/banner/banner-4.png" alt="">
                     <div class="banner-text d-md-block d-none">
                         <h4 class="mb-15 mt-40 text-brand">Repair Services</h4>
-                        <h1 class="fw-600 mb-20">We're an Apple <br>Authorised Service Provider</h1>
+                        <h1 class="fw-600 mb-20 text-brand">We're an Apple <br>Authorised Service Provider</h1>
                         <a href="shop.html" class="btn">Learn More <i class="fi-rs-arrow-right"></i></a>
                     </div>
                 </div>

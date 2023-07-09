c91e5e19e526e0db01a480d03c904f48f4b950bc
anharsaja <anharmuk12@gmail.com>
2023-07-10 00:12:18 +0700

    wishlist remove

---
+++ app/Http/Livewire/WishlistComponent.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class WishlistComponent extends Component
+{
+    public function render()
+    {
+        return view('livewire.wishlist-component');
+    }
+}
---
+++ resources/views/livewire/wishlist-component.blade.php
@@ -0,0 +1,39 @@
+<div>
+    <style>
+        nav svg {
+            height: 20px;
+        }
+
+        nav .hidden {
+            display: block;
+        }
+
+        .wishlisted {
+            background-color: #F15412 !important;
+            border: 1px solid transparent !important;
+        }
+
+        .wishlisted i {
+            color: #fff !important;
+        }
+    </style>
+
+
+    <main class="main">
+        <div class="page-header breadcrumb-wrap">
+            <div class="container">
+                <div class="breadcrumb">
+                    <a href="index.html" rel="nofollow">Home</a>
+                    <span></span> Shop
+                </div>
+            </div>
+        </div>
+        <section class="mt-50 mb-50">
+            <div class="container">
+                <div class="row product-grid-4">
+                    
+                </div>
+            </div>
+        </section>
+    </main>
+</div>

203847cb9188b2af376ec661085a8ac8d18d7c37
anharsaja <anharmuk12@gmail.com>
2023-11-08 13:09:19 +0700

    login update

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -22,7 +22,8 @@
                                         <form method="post" action="{{ route('login') }}">
                                             @csrf
                                             <div class="form-group">
-                                                <input type="text" required="" name="email" placeholder="Your Email" :value="old('email')" required autofocus>
+                                                <!-- <input type="text" required="" name="email" placeholder="Your Email" :value="old('email')" required autofocus> -->
+                                                <input type="text" required="" name="email" placeholder="Your Email" required autofocus>
                                             </div>
                                             <div class="form-group">
                                                 <input required="" type="password" name="password" placeholder="Password" required autocomplete="current-password">
